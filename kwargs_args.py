ceac4465d2d0b4f2d99521317c4b5bdcfbdd7341
ERICK ADIKAH <104826804+Erickadikah@users.noreply.github.com>
2022-09-30 00:16:54 +0300

    kwargs and args

--- kwargs_args.py
+++ kwargs_args.py
@@ -1,6 +1,42 @@
-def test_var_args(f_arg, *argv):
-    print("first normal arg:",).f_arg
-    for arg in argv:
-        print("another arg through *argv :",).arg
+# def test_var_args(f_arg, *argv):
+#     print("first normal arg:",).f_arg
+#     for arg in argv:
+#         print("another arg through *argv :",).arg
+#
+# test_var_args('yasoob','python','eggs','test')
+# def person (name, **data):
+#      print(name)
+#      for i,j in data.items():
+#          print(i,j)
+#
+#
+#
+#
+# person('erick adikah', age=22, city='Nairobi', mob=97901001)
 
-test_var_args('yasoob','python','eggs','test')
+def my_func(*args, **kwargs):
+    print("hello world", args, kwargs)
+
+my_func("abc", key=123, abc=123)
+
+
+# def calcfunvalue(*args):
+#     print(args)
+#     totalvalue = sum(args)
+#     return(totalvalue)
+# print()
+#
+#
+#
+# calcfunvalue(10,20,30,46)
+# print()
+
+
+def news(**kwargs):
+    mystr = kwargs['player'] + "scored" + kwargs['runs'] + "runs"
+    return(mystr)
+    if 'place' in kwargs:
+        mystr = mystr+" in "+kwargs['place']
+
+
+news(player="erick", runs="2000", place="Nairobi")
